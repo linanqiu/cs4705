a8850870d05fac6a01f2d42adb85cb29a5a8ea6c
Linan Qiu <linanqiu@gmail.com>
2014-09-27 21:20:30 -0400

    tab

--- hw1/src/EmissionParameters.java
+++ hw1/src/EmissionParameters.java
@@ -10,80 +10,79 @@ import java.util.Iterator;
 
 public class EmissionParameters {
 
-	public static final String RARETAG = "_RARE_";
-	public static final String WORDTAG = "WORDTAG";
+  public static final String RARETAG = "_RARE_";
+  public static final String WORDTAG = "WORDTAG";
 
-	public static void replaceRare(File ner_train, File ner_train_count)
-			throws IOException {
-		Hashtable<String, Integer> rares = new Hashtable<String, Integer>();
+  public static void replaceRare(File ner_train, File ner_train_count)
+      throws IOException {
+    Hashtable<String, Integer> rares = new Hashtable<String, Integer>();
 
-		// count all words since rare tags are non tag specific
-		BufferedReader bufferedReader = new BufferedReader(new FileReader(
-				ner_train_count));
-		String line;
-		while ((line = bufferedReader.readLine()) != null) {
-			String[] lineArray = line.split(" ");
-			int count = Integer.parseInt(lineArray[0]);
-			if (lineArray[1].equals(WORDTAG)) {
-				if (rares.containsKey(lineArray[3])) {
-					rares.put(lineArray[3], rares.get(lineArray[3]) + count);
-				} else {
-					rares.put(lineArray[3], count);
-				}
-			}
-		}
+    // count all words since rare tags are non tag specific
+    BufferedReader bufferedReader = new BufferedReader(new FileReader(
+        ner_train_count));
+    String line;
+    while ((line = bufferedReader.readLine()) != null) {
+      String[] lineArray = line.split(" ");
+      int count = Integer.parseInt(lineArray[0]);
+      if (lineArray[1].equals(WORDTAG)) {
+        if (rares.containsKey(lineArray[3])) {
+          rares.put(lineArray[3], rares.get(lineArray[3]) + count);
+        } else {
+          rares.put(lineArray[3], count);
+        }
+      }
+    }
 
-		// eliminate all non-rare words
-		Iterator<String> ite = rares.keySet().iterator();
-		while (ite.hasNext()) {
-			String key = ite.next();
-			if (!(rares.get(key) < 5)) {
-				ite.remove();
-			}
-		}
+    // eliminate all non-rare words
+    Iterator<String> ite = rares.keySet().iterator();
+    while (ite.hasNext()) {
+      String key = ite.next();
+      if (!(rares.get(key) < 5)) {
+        ite.remove();
+      }
+    }
 
-		bufferedReader.close();
+    bufferedReader.close();
 
-		// replace rare words with RARETAG
-		String fileName = ner_train.getName().split("\\.")[0] + "_replaced.dat";
-		File replaced = new File(fileName);
+    // replace rare words with RARETAG
+    String fileName = ner_train.getName().split("\\.")[0] + "_replaced.dat";
+    File replaced = new File(fileName);
 
-		bufferedReader = new BufferedReader(new FileReader(ner_train));
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(
-				replaced));
+    bufferedReader = new BufferedReader(new FileReader(ner_train));
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(replaced));
 
-		while ((line = bufferedReader.readLine()) != null) {
-			String[] lineArray = line.split(" ");
-			if (rares.containsKey(lineArray[0])) {
-				line = RARETAG + " " + lineArray[1];
-			}
-			bufferedWriter.write(line + "\n");
-		}
+    while ((line = bufferedReader.readLine()) != null) {
+      String[] lineArray = line.split(" ");
+      if (rares.containsKey(lineArray[0])) {
+        line = RARETAG + " " + lineArray[1];
+      }
+      bufferedWriter.write(line + "\n");
+    }
 
-		bufferedReader.close();
-		bufferedWriter.close();
-	}
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
 
-	public static void namedEntityTagger(File ner_train_replaced_count,
-			File ner_dev) throws IOException {
-		// total counts
-		Hashtable<String, Integer> counts = new Hashtable<String, Integer>();
-		// conditional counts
-		Hashtable<String, Integer> conditionalCounts = new Hashtable<String, Integer>();
+  public static void namedEntityTagger(File ner_train_replaced_count,
+      File ner_dev) throws IOException {
+    // total counts
+    Hashtable<String, Integer> counts = new Hashtable<String, Integer>();
+    // conditional counts
+    Hashtable<String, Integer> conditionalCounts = new Hashtable<String, Integer>();
 
-		BufferedReader bufferedReader = new BufferedReader(new FileReader(
-				ner_train_replaced_count));
-		String line;
-		while ((line = bufferedReader.readLine()) != null) {
-			String[] lineArray = line.split(" ");
-			if (lineArray[1].equals(WORDTAG)) {
-				int count = Integer.parseInt(lineArray[0]);
-				if (counts.containsKey(lineArray[2])) {
-					counts.put(lineArray[1], counts.get(lineArray[1]) + count);
-				} else {
-					counts.put(lineArray[1], count);
-				}
-			}
-		}
-	}
+    BufferedReader bufferedReader = new BufferedReader(new FileReader(
+        ner_train_replaced_count));
+    String line;
+    while ((line = bufferedReader.readLine()) != null) {
+      String[] lineArray = line.split(" ");
+      if (lineArray[1].equals(WORDTAG)) {
+        int count = Integer.parseInt(lineArray[0]);
+        if (counts.containsKey(lineArray[2])) {
+          counts.put(lineArray[1], counts.get(lineArray[1]) + count);
+        } else {
+          counts.put(lineArray[1], count);
+        }
+      }
+    }
+  }
 }
--- hw1/src/Test.java
+++ hw1/src/Test.java
@@ -2,13 +2,13 @@ import java.io.File;
 import java.io.IOException;
 
 public class Test {
-	public static void main(String[] args) {
-		try {
-			EmissionParameters.replaceRare(new File("ner_train.dat"), new File(
-					"ner.counts"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+  public static void main(String[] args) {
+    try {
+      EmissionParameters.replaceRare(new File("ner_train.dat"), new File(
+          "ner.counts"));
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 }
